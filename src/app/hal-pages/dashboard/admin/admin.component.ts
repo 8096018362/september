cdb9339f9f6e8a1030d1532c20c8765244a5bc4a
ramu vishwanatham <ramu@lifelinelab.io>
2021-09-27 20:24:30 +0530

    chart added

--- src/app/hal-pages/dashboard/admin/admin.component.ts
+++ src/app/hal-pages/dashboard/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 declare const $: any;
+declare const ApexCharts: any;
+
 @Component({
     selector: '',
     templateUrl: './admin.component.html',
@@ -19,5 +21,89 @@ export class AdminComponent {
                 responsive: true
             });
         }, 1000);
+        this.chart1()
+    }
+    private chart1() {
+        var options = {
+            chart: {
+                height: 350,
+                type: 'bar',
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: false,
+                    endingShape: 'rounded',
+                    distributed: true,
+                    columnWidth: '25%',
+                    color: {
+                        backgroundBarColors: ['#F9A44A', '#2EAFBC', '#F35F67', '#5A36CC'],
+                    }
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            series: [{
+                name: '',
+                data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+            }],
+            xaxis: {
+                categories: ['Tupendane', 'Huruma', '5G Capital', 'ABC Expat', 'Angama Mara', 'Arifu', 'Aspira Kenya', 'Bliss Resort', 'Bomi E&C'],
+                labels: {
+                    style: {
+                        colors: '#52504F',
+                        fontSize: '18px',
+                        fontWeight: 'bold',
+                        fontFamily: 'Source Sans Pro sans-serif !important',
+                    }
+                }, title: {
+                    text: '',
+                    style: {
+                        fontSize: '22px',
+                        fontWeight: 'bold',
+                        fontFamily: 'Source Sans Pro sans-serif !important',
+                        color: '#585555'
+                    },
+                },
+            },
+            yaxis: {
+                labels: {
+                    style: {
+                        colors: '#52504F',
+                        fontSize: '18px',
+                        fontWeight: 'bold',
+                        fontFamily: 'Source Sans Pro sans-serif !important',
+                    }
+                }
+            },
+            fill: {
+                opacity: 1
+
+            },
+            tooltip: {
+                y: {
+                    formatter: function (val) {
+                        return "$ " + val + " thousands"
+                    }
+                }
+            }
+        }
+
+        var chart = new ApexCharts(
+            document.querySelector("#chart1"),
+            options
+        );
+
+        chart.render();
+
+
     }
 }
