0e01c7e2c2d5377a02c83551481fe697ceffa850
saisruthiroyal <sowkya27@gmail.com>
2021-09-28 21:20:24 +0530

    validation messages in employer

--- src/app/common/validation-messages.ts
+++ src/app/common/validation-messages.ts
@@ -9,4 +9,31 @@ export const ValidationMessages = {
         { type: 'required', message: 'National ID is required' }
     ],
 
+    employer_name: [
+        { type: 'required', message: 'Name of Employer is required' }
+    ],
+
+    reg_no: [
+        { type: 'required', message: 'Registration No. Of Business is required' }
+    ],
+    employer_id: [ { type: 'required', message: 'Employer ID is required' }],
+    reg_certificate: [ { type: 'required', message: 'Registration Certificate is required' }],
+    max_fund: [ { type: 'required', message: 'Maximum Fund Per Employee is required' }],
+    business_type: [ { type: 'required', message: 'Type of Business is required' }],
+    approved_fund: [ { type: 'required', message: 'Approved Fund Per Employee is required' }],
+    no_of_employees: [ { type: 'required', message: 'No. Of Employees is required' }],
+    no_of_employees_finance: [ { type: 'required', message: 'No. Of Employee For Finance is required' }],
+    country: [ { type: 'required', message: 'Country is required' }],
+    state: [ { type: 'required', message: 'State is required' }],
+    address1: [ { type: 'required', message: 'Address1 is required' }],
+    address2: [ { type: 'required', message: 'Address2 is required' }],
+    address3: [ { type: 'required', message: 'Address3 is required' }],
+    pincode: [ { type: 'required', message: 'PIN is required' }],
+    latitude: [ { type: 'required', message: 'Latitude is required' }],
+    longitude: [ { type: 'required', message: 'Longitude is required' }],
+    dir_national_id: [ { type: 'required', message: 'Director’s National ID is required' }],
+    dir_natonal_id_copy: [ { type: 'required', message: 'Director’s National ID Copy is required' }],
+    contact_person: [ { type: 'required', message: 'Contact Person is required' }],
+    status: [ { type: 'required', message: 'Status is required' }]
+
 };
--- src/app/hal-pages/employer/add/add.component.ts
+++ src/app/hal-pages/employer/add/add.component.ts
@@ -1,4 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ValidationMessages } from "src/app/common/validation-messages";
+import { ValidationPatterns } from "src/app/common/validation-patterns";
+
 
 @Component({
   selector: '',
@@ -7,6 +11,49 @@ import { Component } from "@angular/core";
 
 })
 
-export class AddComponent {
+export class AddComponent implements  OnInit{
+  addEmployerForm:FormGroup;
+  validation_messages: any = {};
+  pattens: any = {};
+  submitted:boolean=false;
+
+  constructor(public formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(){
+    this.validation_messages = ValidationMessages;
+    this.pattens = ValidationPatterns;
+
+
+    this.addEmployerForm = this.formBuilder.group({
+        employer_name: [null, this.pattens.required],
+        reg_no: [null, this.pattens.required],
+        employer_id: [null, this.pattens.required],
+        reg_certificate: [null, this.pattens.required],
+        max_fund: [null, this.pattens.required],
+        business_type: [null, this.pattens.required],
+        approved_fund: [null, this.pattens.required],
+        no_of_employees: [null, this.pattens.required],
+        no_of_employees_finance: [null, this.pattens.required],
+        country: [null, this.pattens.required],
+        state: [null, this.pattens.required],
+        address1: [null, this.pattens.required],
+        address2: [null, this.pattens.required],
+        address3: [null, this.pattens.required],
+        pincode: [null, this.pattens.required],
+        latitude: [null, this.pattens.required],
+        longitude: [null, this.pattens.required],
+        dir_national_id: [null, this.pattens.required],
+        dir_natonal_id_copy: [null, this.pattens.required],
+        contact_person: [null, this.pattens.required],
+        status: [null, this.pattens.required]
+    });
+  }
+
+  addEmployer(){
+    this.submitted = true;
+    console.log(this.addEmployerForm.value)
+  }
 
 }
