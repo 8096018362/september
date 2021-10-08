fd84b383ba9c3c4642adaca0d02e78e27c69116f
saisruthiroyal <sowkya27@gmail.com>
2021-10-09 00:02:34 +0530

    table collapse design

--- src/app/hal-pages/finance-origination/list/list.component.ts
+++ src/app/hal-pages/finance-origination/list/list.component.ts
@@ -95,20 +95,57 @@ export class ListComponent {
             /* Formatting function for row details - modify as you need */
             function format(d) {
                 // `d` is the original data object for the row
-                return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;background:#f5f5f5;">' +
-                    '<tr>' +
-                    '<td>Full name:</td>' +
-                    '<td>' + d.name + '</td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td>Extension number:</td>' +
-                    '<td>' + d.approveAmount + '</td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td>Extra info:</td>' +
-                    '<td>And any further details here (images etc)...</td>' +
-                    '</tr>' +
-                    '</table>';
+                return '<div class="containerdiv">' +
+                    ' <div class="row">' +
+                    '<div class="col-md-12">'+
+                    '<h4>Employee Details</h4>'+
+                    '  </div>'+
+                    '  </div>'+
+                    ' <div class="rowdiv">'+
+                    ' <div class="carddiv">'+
+                    '<h5>Employee Name</h5>' +
+                    '<p>' + d.name + '</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Finance ID</h5>' +
+                    '<p>' + d.approveAmount + '</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>National ID</h5>' +
+                    '<p>' + d.approveAmount + '</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Phone Number</h5>' +
+                    '<p>' + d.approveAmount + '</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Finance Amount Requested</h5>' +
+                    '<p>67890</p>' +
+                    '</div>' +
+                    '</div>' +
+                    ' <div class="rowdiv">'+
+                    ' <div class="carddiv">'+
+                    '<h5>Request Date</h5>' +
+                    '<p>22/06/2021</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Approved Date</h5>' +
+                    '<p>22/06/2021</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Finance Start Date</h5>' +
+                    '<p>22/06/2021</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>EMI Amount</h5>' +
+                    '<p>' + d.approveAmount + '</p>' +
+                    '</div>' +
+                    ' <div class="carddiv">' +
+                    '<h5>Status</h5>' +
+                    '<p class="finstatus_approv">Approved</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '</div>';
             }
 
 
@@ -116,23 +153,24 @@ export class ListComponent {
             var table = $('#chieldRow').DataTable({
                 data: childData,
                 "columns": [
+                   
+                    { "data": "name" },
+                    { "data": "requestPending" },
+                    { "data": "requestApprove" },
+                    { "data": "requestAmount" },
+                    { "data": "approveAmount" },
                     {
                         "className": 'details-control',
                         "orderable": false,
                         "data": null,
                         "defaultContent": ''
-                    },
-                    { "data": "name" },
-                    { "data": "requestPending" },
-                    { "data": "requestApprove" },
-                    { "data": "requestAmount" },
-                    { "data": "approveAmount" }
+                    }
                 ],
                 "order": [[1, 'asc']]
             });
 
             // Add event listener for opening and closing details
-            $('#chieldRow tbody').on('click', 'td.details-control', function () {
+            $('#chieldRow tbody').on('click', 'td', function () {
                 var tr = $(this).closest('tr');
                 var row = table.row(tr);
 
