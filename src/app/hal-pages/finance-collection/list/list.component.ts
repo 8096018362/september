7516b3607f98cb4794a4d522bbba6561f2b65a23
ramu vishwanatham <ramu@lifelinelab.io>
2021-10-19 00:42:55 +0530

    ui boxes added in fiance collection

--- src/app/hal-pages/finance-collection/list/list.component.ts
+++ src/app/hal-pages/finance-collection/list/list.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: '',
     templateUrl: './list.component.html',
-    //  styleUrls: ['./list.component.scss']
+    styleUrls: ['./list.component.scss']
 
 })
 
