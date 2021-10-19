7ebf234efe4963cc029e7cc8ab9d37d4126050c0
ramu vishwanatham <ramu@lifelinelab.io>
2021-10-19 19:11:04 +0530

    notification module added

--- src/app/hal-pages/notifications/list/list.component.ts
+++ src/app/hal-pages/notifications/list/list.component.ts
@@ -4,150 +4,17 @@ declare const $: any;
 @Component({
     selector: '',
     templateUrl: './list.component.html',
-      styleUrls: ['./list.component.scss']
+    styleUrls: ['./list.component.scss']
 
 })
 
 export class ListComponent {
-    constructor() { }
+    showTemplate = 'SMS'
+    constructor( ) { }
 
-    ngOnInit() {
-        childRowTable();
-
-        function childRowTable() {
-            var childData = [
-                {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                }, {
-                    "id": "1",
-                    "name": "Tiger Nixon",
-                    "requestPending": "1",
-                    "requestApprove": "2",
-                    "requestAmount": "KES 2000",
-                    "approveAmount": "KES 2000"
-                },
-
-            ];
-
-
-
-            /* Formatting function for row details - modify as you need */
-            function format(d) {
-                // `d` is the original data object for the row
-                return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;background:#f5f5f5;">' +
-                    '<tr>' +
-                    '<td>Full name:</td>' +
-                    '<td>' + d.name + '</td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td>Extension number:</td>' +
-                    '<td>' + d.approveAmount + '</td>' +
-                    '</tr>' +
-                    '<tr>' +
-                    '<td>Extra info:</td>' +
-                    '<td>And any further details here (images etc)...</td>' +
-                    '</tr>' +
-                    '</table>';
-            }
-
-
-
-            var table = $('#chieldRow').DataTable({
-                data: childData,
-                "columns": [
-                    {
-                        "className": 'details-control',
-                        "orderable": false,
-                        "data": null,
-                        "defaultContent": ''
-                    },
-                    { "data": "name" },
-                    { "data": "requestPending" },
-                    { "data": "requestApprove" },
-                    { "data": "requestAmount" },
-                    { "data": "approveAmount" }
-                ],
-                "order": [[1, 'asc']]
-            });
-
-            // Add event listener for opening and closing details
-            $('#chieldRow tbody').on('click', 'td.details-control', function () {
-                var tr = $(this).closest('tr');
-                var row = table.row(tr);
-
-                if (row.child.isShown()) {
-                    // This row is already open - close it
-                    row.child.hide();
-                    tr.removeClass('shown');
-                }
-                else {
-                    // Open this row
-                    row.child(format(row.data())).show();
-                    tr.addClass('shown');
-                }
-            });
-
-        }
+    ngOnInit() { }
+    selectTnype(req){
+        console.log(req);
+        this.showTemplate = req;
     }
 }
